e50dfe047a32624914a73795c99e525c5bdd460f
Урматай <urmatai.toktosunova@alatoo.edu.kg>
2024-03-26 11:14:22 +0600

    repositories implemented

---
+++ src/main/java/org/example/taskmanager/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package org.example.taskmanager.repositories;
+
+import org.example.taskmanager.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
---
+++ src/main/java/org/example/taskmanager/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.taskmanager.repositories;
+
+import org.example.taskmanager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
