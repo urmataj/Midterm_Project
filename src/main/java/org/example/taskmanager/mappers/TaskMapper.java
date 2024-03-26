b85574bb8606c12adf4638fdea177b9b45a8c6d5
Урматай <urmatai.toktosunova@alatoo.edu.kg>
2024-03-26 12:35:31 +0600

    mappers implemented

---
+++ src/main/java/org/example/taskmanager/mappers/TaskMapper.java
@@ -0,0 +1,29 @@
+package org.example.taskmanager.mappers;
+
+import org.example.taskmanager.dtos.TaskDTO;
+import org.example.taskmanager.entities.Task;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapper {
+    public TaskDTO toDto(Task task) {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId(task.getId());
+        taskDTO.setTitle(task.getTitle());
+        taskDTO.setDescription(task.getDescription());
+        taskDTO.setCompleted(task.isCompleted());
+        taskDTO.setUserId(task.getUser().getId());
+
+        return taskDTO;
+    }
+
+    public Task toEntity(TaskDTO taskDTO) {
+        Task task = new Task();
+        task.setId(taskDTO.getId());
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setCompleted(taskDTO.isCompleted());
+
+        return task;
+    }
+}
---
+++ src/main/java/org/example/taskmanager/mappers/UserMapper.java
@@ -0,0 +1,24 @@
+package org.example.taskmanager.mappers;
+
+import org.example.taskmanager.dtos.UserDTO;
+import org.example.taskmanager.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserDTO toDto(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+        return userDTO;
+    }
+
+    public User toEntity(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        return user;
+    }
+}
